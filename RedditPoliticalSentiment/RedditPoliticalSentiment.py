da090f972596ed060ba0b7d022ac5cb75ff699d5
Faiz Surani <faiz.surani@gmail.com>
2019-04-22 10:46:05 -0700

    Resolved CSV formatting issues, will now begin concurrent collection

--- RedditPoliticalSentiment/RedditPoliticalSentiment.py
+++ RedditPoliticalSentiment/RedditPoliticalSentiment.py
@@ -3,6 +3,8 @@ import csv
 import datetime
 from multiprocessing import Process
 
+#On Reddit and the Reddit API, adding subreddits together in the url such that you have r/sub1+sub2+...
+#treats the resulting combination of subs as one single subreddit, allowing me to scrape all of them simultaneously
 def createMultiSubString(subArr):
     multiString = ""
     for sub in subArr:
@@ -11,24 +13,25 @@ def createMultiSubString(subArr):
     return multiString
 
 def collectSubComments(fileName, subString): 
-    with open(fileName + '.csv', 'w',newline='',encoding='utf-8') as csvfile:
+    with open(fileName + '.csv', 'w',newline='') as csvfile:
         filewriter = csv.writer(csvfile, delimiter=',',
-                                quotechar='|', escapechar='\\', quoting=csv.QUOTE_ALL)
+                                quotechar='"', escapechar='\\', quoting=csv.QUOTE_ALL)
         filewriter.writerow(['Comment', 'Subreddit','Date Created','Author','ID'])
         for comment in reddit.subreddit(subString).stream.comments():
-            commentBodySansFormatting = comment.body.encode('utf-8', errors='ignore')
+            commentBodySansFormatting = comment.body.encode('ascii', errors='ignore')
             filewriter.writerow([commentBodySansFormatting, comment.subreddit, comment.created_utc, comment.author, comment.id])
 
-reddit = praw.Reddit()
-print(reddit.read_only)
+def main():
+    reddit = praw.Reddit()
+    print(reddit.read_only)
 
-liberalSubreddits = ["neoliberal","LateStageCapitalism","SandersForPresident","socialism","EnoughTrumpSpam","progressive"]
-conservativeSubreddits = ["the_donald","Conservative","askthe_donald","libertarian","TheNewRight"]
+    liberalSubreddits = ["neoliberal","LateStageCapitalism","SandersForPresident","socialism","EnoughTrumpSpam","progressive"]
+    conservativeSubreddits = ["the_donald","Conservative","askthe_donald","libertarian","TheNewRight"]
 
-libString = createMultiSubString( liberalSubreddits)
-conString = createMultiSubString(conservativeSubreddits)
-print(libString)
+    libString = createMultiSubString( liberalSubreddits)
+    conString = createMultiSubString(conservativeSubreddits)
+    print(libString)
 
-collectSubComments('LiberalSubComments', libString)
-collectSubComments('ConservativeSubComments', conString)
+    collectSubComments('LiberalSubComments', libString)
+    collectSubComments('ConservativeSubComments', conString)
 
