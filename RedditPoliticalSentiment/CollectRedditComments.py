74d14af517856a6117e8982937845db4908039ab
Faiz Surani <faiz.surani@gmail.com>
2019-04-22 11:53:13 -0700

    Switched to PyCharm, cleaned up formatting

--- RedditPoliticalSentiment/CollectRedditComments.py
+++ RedditPoliticalSentiment/CollectRedditComments.py
@@ -2,41 +2,46 @@ import praw
 import csv
 import time
 from multiprocessing import Process
+import unidecode
 
-#On Reddit and the Reddit API, adding subreddits together in the url such that you have r/sub1+sub2+... treats the resulting combination of subs as one single subreddit, allowing me to scrape all of them simultaneously
+
+# On Reddit and the Reddit API, adding subreddits together in the url such that you have r/sub1+sub2+... treats the
+# resulting combination of subs as one single subreddit, allowing me to scrape all of them simultaneously
 def createMultiSubString(subArr):
     multiString = ""
     for sub in subArr:
-        multiString +=  "+" + sub
+        multiString += "+" + sub
     multiString = multiString[1:]
     return multiString
 
-def collectSubComments(fileName, subString): 
-    with open(fileName + '.csv', 'w',newline='') as csvfile:
+
+def collectSubComments(fileName, subString):
+    with open(fileName + '.csv', 'w', newline='', encoding='ascii') as csvfile:
         filewriter = csv.writer(csvfile, delimiter=',',
                                 quotechar='"', escapechar='\\', quoting=csv.QUOTE_ALL)
-        filewriter.writerow(['Comment', 'Subreddit','Date Created','Author','ID'])
+        filewriter.writerow(['Comment', 'Subreddit', 'Date Created', 'Author', 'ID'])
         for comment in reddit.subreddit(subString).stream.comments():
-            commentBodySansFormatting = comment.body.encode('ascii', errors='ignore')
-            filewriter.writerow([commentBodySansFormatting, comment.subreddit, comment.created_utc, comment.author, comment.id])
+            commentBodySansFormatting = unidecode.unidecode(comment.body)
+            filewriter.writerow(
+                [commentBodySansFormatting, comment.subreddit, comment.created_utc, comment.author, comment.id])
 
 
 reddit = praw.Reddit()
 print(reddit.read_only)
 
-liberalSubreddits = ["neoliberal","LateStageCapitalism","SandersForPresident","socialism","EnoughTrumpSpam","progressive"]
-conservativeSubreddits = ["the_donald","Conservative","askthe_donald","libertarian","TheNewRight"]
+liberalSubreddits = ["neoliberal", "LateStageCapitalism", "SandersForPresident", "socialism", "EnoughTrumpSpam",
+                     "progressive"]
+conservativeSubreddits = ["the_donald", "Conservative", "askthe_donald", "libertarian", "TheNewRight"]
 
-libString = createMultiSubString( liberalSubreddits)
+libString = createMultiSubString(liberalSubreddits)
 conString = createMultiSubString(conservativeSubreddits)
 
-libProcess = Process(target=collectSubComments,args=('LiberalSubComments', libString))
-conProcess = Process(target=collectSubComments,args=('ConservativeSubComments', conString))
+libProcess = Process(target=collectSubComments, args=('LiberalSubComments', libString))
+conProcess = Process(target=collectSubComments, args=('ConservativeSubComments', conString))
 if __name__ == '__main__':
     libProcess.start()
     conProcess.start()
 
-    time.sleep(259200)
+    time.sleep(10)
     libProcess.terminate()
     conProcess.terminate()
-
