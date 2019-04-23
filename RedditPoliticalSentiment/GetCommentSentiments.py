95cb8e436383f682d3d5cbba1f3d004f76bd8dfb
Faiz Surani <faiz.surani@gmail.com>
2019-04-23 00:57:54 -0700

    Made sentiment queries rate-limit compliant. Ready to go once data is finished collecting.

--- RedditPoliticalSentiment/GetCommentSentiments.py
+++ RedditPoliticalSentiment/GetCommentSentiments.py
@@ -1,3 +1,4 @@
+import time
 from statistics import mean
 
 import CreateCommentSamples
@@ -13,13 +14,28 @@ def getNetSentiments(commentArray):
     print(jsonResponse)
     netSentimentScores = []
     for sentimentScores in jsonResponse['sentiment']:
+        # Subtract positivity probability from negative prob to get net sentiment score. Neutral does not need
+        # consideration because it would reduce the extremity of results by taking away from pos and neg scores.
         netScore = float(sentimentScores['positive']) - float(sentimentScores['negative'])
         netSentimentScores.append(netScore)
     return netSentimentScores
 
 
-liberalSample = CreateCommentSamples.getRandomSample(100, "LiberalSubCommentsTest.csv")
+def netSentimentsWithRateLimit(requestsPerMinuteAllowed, commentArray):
+    numberOfQueries = int(len(commentArray) / requestsPerMinuteAllowed) + 1
+    allNetSentiments = []
+    for i in range(numberOfQueries):
+        netSentimentsPortion = getNetSentiments(
+            commentArray[i * requestsPerMinuteAllowed: i * requestsPerMinuteAllowed + requestsPerMinuteAllowed])
+        allNetSentiments.extend(netSentimentsPortion)
+        if (i != numberOfQueries - 1):
+            time.sleep(65)  # Rate limit resets every minute, using 65 seconds just to be safe
+    return allNetSentiments
+
+
+liberalSample = CreateCommentSamples.getRandomSample(14, "LiberalSubCommentsTest.csv")
 liberalCommentArr = CreateCommentSamples.convertSampleToArray(liberalSample).tolist()
-netLiberalSentiments = getNetSentiments(liberalCommentArr)
+# Rate limit is approx 20 requests/min, using 10 just in case because long comments can count for multiple requests
+netLiberalSentiments = netSentimentsWithRateLimit(15, liberalCommentArr)
 meanLiberalSentiment = mean(netLiberalSentiments)
-print(meanLiberalSentiment)
+print("Mean Liberal Sentiment: " + str(meanLiberalSentiment))
